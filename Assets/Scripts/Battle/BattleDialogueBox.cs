8e8f31204a2ece3772349efcb7634381e36203ba
Jorge <jsgonzales10@up.edu.ph>
2023-10-17 16:45:53 +0800

    Merge branch 'MergeBattleOptions' into BattleOptions

---
+++ Assets/Scripts/Battle/BattleDialogueBox.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum BattleState { Start, PlayerAction, EnemyMove, Busy, PlayerMove }
+public class BattleDialogueBox : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI dialogText;
+    [SerializeField] int lettersPerSecond;
+    [SerializeField] Color highlightedColor;
+
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<TextMeshProUGUI> actionTexts;
+    [SerializeField] List<TextMeshProUGUI> moveTexts;
+
+    [SerializeField] TextMeshProUGUI attackText;
+    [SerializeField] TextMeshProUGUI typeText;
+
+
+
+
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;
+    }
+
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / 30);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for (int i = 0; i < actionTexts.Count; i++)
+        {
+            if (i == selectedAction)
+            {
+                actionTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                actionTexts[i].color = Color.black;
+            }
+        }
+    }
+
+    public void UpdateMoveSelection(int selectedMove, Moves move)
+    {
+        for (int i = 0; i < moveTexts.Count; i++)
+        {
+            if (i == selectedMove)
+            {
+                moveTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                moveTexts[i].color = Color.black;
+            }
+
+            attackText.text = "Zinger Steak";
+            typeText.text = "Amazing";
+        }
+    }
+
+    public void SetMoveNames(List<Moves> moves)
+    {
+        for (int i = 0; i <= moves.Count; ++i)
+        {
+            if (i < moves.Count)
+            {
+                moveTexts[i].text = moves[i].Base.MoveName;
+            }
+            else
+            {
+                moveTexts[i].text = "-";
+            }
+        }
+    }
+}
