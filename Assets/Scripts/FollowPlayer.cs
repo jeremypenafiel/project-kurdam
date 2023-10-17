040003593568e705432d72d8d4cf75a79a6a3e7e
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2023-10-17 11:25:48 +0800

    added camera script
    added script for main camera that follows the player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    private void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 1, -5);
+    }
+}
