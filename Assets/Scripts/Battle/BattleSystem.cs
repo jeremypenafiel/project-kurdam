2440c5312cdb3563484cb7ac9f0c2b64d3450451
Jorge <jsgonzales10@up.edu.ph>
2023-10-17 16:49:31 +0800

    Update BattleSystem.cs

--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -1,21 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
     [SerializeField] BattleHud playerHud;
+    [SerializeField] BattleUnit enemyUnit;
+    [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogueBox dialogBox;
 
+    BattleState state;
+    int currentAction;
+    int currentMove;
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    public IEnumerator SetupBattle()
     {
         playerUnit.Setup();
         playerHud.SetData(playerUnit.aswang);
 
+        dialogBox.SetMoveNames(playerUnit.aswang.moves);
+
+        yield return dialogBox.TypeDialog($"A wild {playerUnit.aswang.Base.aname} appeared.");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+
     }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose An Action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+
+
+    }
+    private void Update()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+
+        else if (state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+
+    void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentAction < 1)
+            {
+                ++currentAction;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction > 0)
+            {
+                --currentAction;
+            }
+        }
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                PlayerMove();
+            }
+            else if (currentAction == 1)
+            {
+                //run
+            }
+        }
+    }
+
+    void HandleMoveSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentMove < playerUnit.aswang.moves.Count - 1)
+            {
+                ++currentMove;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentMove > playerUnit.aswang.moves.Count - 1)
+            {
+                --currentMove;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentMove < playerUnit.aswang.moves.Count - 2)
+            {
+                currentMove += 2;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentMove > 1)
+            {
+                currentMove -= 2;
+            }
+        }
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.aswang.moves[currentMove]);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentMove == 0)
+            {
+                //
+            }
+            else if (currentMove == 1)
+            {
+                //run
+            }
+        }
+
+
+    }
+
 }
