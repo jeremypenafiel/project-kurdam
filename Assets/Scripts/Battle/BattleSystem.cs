210903f86ee211fb513ccbe894e0c2a84f2d53e2
vjauxysbrynxx <90746160+vjauxysbrynxx@users.noreply.github.com>
2023-10-23 16:14:17 +0800

    Edited and Fixed

--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System;
+
 
 public enum BattleState { Start, PlayerAction, EnemyMove, Busy, PlayerMove, PlayerAttackRoll, PlayerDamageRoll, EnemyAttackRoll, EnemyDamageRoll }
 
