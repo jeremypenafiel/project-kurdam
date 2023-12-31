f197a505b6a448ba531a5978da0feae0ebb700ac
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2023-12-31 21:01:01 +0800

    create files

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicPlayer;
+    [SerializeField] AudioSource sfxPlayer;
+
+    public void PlayMusic(AudioClip clip, bool loop = true)
+    {
+
+    }
+
+}
+
+
