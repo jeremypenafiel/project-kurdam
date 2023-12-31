205528cea9b96cede85391409e95296b2f867242
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2023-12-31 21:15:58 +0800

    Update AudioManager.cs

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -7,9 +7,21 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource musicPlayer;
     [SerializeField] AudioSource sfxPlayer;
 
+    public static AudioManager i { get; private set; }
+
+
+    private void Awake()
+    {
+        i = this; 
+    }
+
     public void PlayMusic(AudioClip clip, bool loop = true)
     {
+        if (clip == null) return;
 
+        musicPlayer.clip = clip;
+        sfxPlayer.loop = loop;
+        musicPlayer.Play();
     }
 
 }
