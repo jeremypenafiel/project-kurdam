9727ae59e497f288a72ff00e38fd2b3a58e32df0
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2024-01-09 02:20:25 +0800

    working i think

--- Assets/Scripts/SceneObjects/Book.cs
+++ Assets/Scripts/SceneObjects/Book.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Book : MonoBehaviour, Interactable
 {
+    [SerializeField] Dialog dialog;
+    [SerializeField] string line;
+    [SerializeField] Sprite closed;
+    [SerializeField] Sprite open;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = closed;
+    }
     public IEnumerator Interact(Transform initiator)
     {
+        GetComponent<SpriteRenderer>().sprite = open;
         Debug.Log("Interacting with book");
-        yield return null;
+        yield return DialogManager.Instance.ShowDialog(dialog);
+        GetComponent<SpriteRenderer>().sprite = closed;
     }
 }
