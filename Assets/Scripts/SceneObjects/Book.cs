0f49c34bbbe9bac1a1bdc93fcf4b7f492afd19d0
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2024-01-09 00:54:13 +0800

    add book

--- Assets/Scripts/SceneObjects/Book.cs
+++ Assets/Scripts/SceneObjects/Book.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Book : MonoBehaviour
+public class Book : MonoBehaviour, Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    public IEnumerator Interact(Transform initiator)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Interacting with book");
+        yield return null;
     }
 }
