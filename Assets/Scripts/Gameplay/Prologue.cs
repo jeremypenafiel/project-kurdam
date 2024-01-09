d60834bae1ea97c58cf9473f3eedc0aa70f63228
vjauxysbrynxx <90746160+vjauxysbrynxx@users.noreply.github.com>
2024-01-09 12:15:48 +0800

    PrologueLoading

---
+++ Assets/Scripts/Gameplay/Prologue.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Prologue : MonoBehaviour
+{
+    Fader fader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine(FinishPrologue());
+       fader = FindObjectOfType<Fader>();
+    }
+
+    IEnumerator FinishPrologue()
+    {
+
+        yield return new WaitUntil(()=>Input.GetKeyDown(KeyCode.Z));
+        yield return fader.FadeIn(0.5f);
+        yield return SceneManager.LoadSceneAsync(2);
+        yield return fader.FadeOut(0.5f);    
+    }
+    
+}
