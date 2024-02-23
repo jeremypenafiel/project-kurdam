713c5a69c3839c7f334c199d92dd2de555d7d3c7
Jorge <jsgonzales10@up.edu.ph>
2024-02-24 01:19:59 +0800

    RandomlySpawnEnemy
    Randomly selects spawn area and generate enemy randomly within the area. Each area has unique monster to spawn.

--- Assets/Scripts/Gameplay/CreateEnemy.cs
+++ Assets/Scripts/Gameplay/CreateEnemy.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class CreateEnemy : MonoBehaviour
 {
-    public GameObject enemy;
+
+    public GameObject spawner;
+    public List<Transform> spawnList;
+    public int maxspawn;
+
+    private GameObject enemy;
+    private int num =0;
+    private Transform spawn;
+    private EnemySpawn enemySpawn;
     // Start is called before the first frame update
     void Start()
     {
-        for (var i = 0; i < 3; i++)
+        
+        while (num < maxspawn){
+            if (Random.Range(0,100)<50)
+            {
+                var rnum = Random.Range(0, spawnList.Count);
+                spawn = spawnList[rnum];
+                enemySpawn= spawn.GetComponent<EnemySpawn>();
+                enemy = enemySpawn.enemy;
+
+                gameObject.transform.position = new Vector2(spawn.transform.position.x, spawn.transform.position.y);
+                var Xsize = spawn.GetComponent<BoxCollider2D>().size.x / 2;
+                var Ysize = spawn.GetComponent<BoxCollider2D>().size.y / 2;
+                var xLocation = Random.Range(gameObject.transform.position.x - Xsize, gameObject.transform.position.x + Xsize + 1);
+                var yLocation = Random.Range(gameObject.transform.position.y - Ysize, gameObject.transform.position.y + Ysize + 1);
+
+                var randomPosition = new Vector3(xLocation,yLocation, 0);
+                Instantiate(enemy, randomPosition, Quaternion.identity);
+                num++;
+            }
+        }
+        /*for (var i = 0; i < 3; i++)
         {
             Instantiate(enemy, new Vector3(gameObject.transform.position.x-5*i, gameObject.transform.position.y, 0), Quaternion.identity);
-        }
+        }*/
     }
 
     // Update is called once per frame
