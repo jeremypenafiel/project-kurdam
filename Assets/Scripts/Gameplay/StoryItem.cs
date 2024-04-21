d87b9daa14964efcd59ac229479c8d62a9b338fc
Jorge <jsgonzales10@up.edu.ph>
2024-04-22 04:21:49 +0800

    QuestSystem
    -Added interaction between story item and quests
    -Added code for quests execution, requirement, and completetion reward
    -Need access to inventory
    -

--- Assets/Scripts/Gameplay/StoryItem.cs
+++ Assets/Scripts/Gameplay/StoryItem.cs
@@ -5,11 +5,51 @@ using UnityEngine;
 public class StoryItem : MonoBehaviour, IPLayerTriggerable
 {
     [SerializeField] Dialog dialog;
+    [SerializeField] QuestBase questToStart;
+    [SerializeField] QuestBase questToComplete;
+
+
+    Quest activeQuest;
    public void OnPlayerTriggered(PlayerController player)
     {
         player.Character.Animator.IsMoving = false;
         StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+
+
     }
 
+    private IEnumerator Start()
+    {
+        if (questToComplete!=null)
+        {
+            var quest = new Quest(questToComplete);
+            yield return quest.CompletedQuest();
+            questToComplete = null;
+        }
+        if (questToStart != null)
+        {
+            activeQuest = new Quest(questToStart);
+            yield return activeQuest.StartQuest();
+
+            if (activeQuest.CanBeCompleted())
+            {
+                yield return activeQuest.CompletedQuest();
+                activeQuest = null;
+            }
+        }
+
+        else if (activeQuest != null)
+        {
+            if(activeQuest.CanBeCompleted())
+            {
+                yield return activeQuest.CompletedQuest();
+                activeQuest = null;
+            }
+            else
+            {
+                yield return DialogManager.Instance.ShowDialog(activeQuest.Base.InprogressDialogue);
+            }
+        }
+    }
     public bool TriggerRepeatedly=> false;
 }
